960591418acd9c3247af4a69a1c733afb6fd4f28
Christophe <Christophe@Christophe.fr>
2023-12-19 23:21:32 +0100

    Modif MDP 12341234

--- conservatoire.py
+++ conservatoire.py
@@ -22,7 +22,7 @@ rowsPins = [18, 23, 24, 25]     #connect to the row pinouts of the keypad
 colsPins = [10, 22, 27, 17]     #connect to the column pinouts of the keypad
 
 # Variables
-reponse = "1234"
+reponse = "12341234"
 styles = ["Classique", "Jazz", "Country", "Reggae"]
 
 def buzztouches():
