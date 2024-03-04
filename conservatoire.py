5b43ce6fc8532b415a66dd910e60e99cd04392b4
Christophe <Christophe@Christophe.fr>
2024-03-04 15:02:42 +0100

    nouvel essai

--- conservatoire.py
+++ conservatoire.py
@@ -69,8 +69,8 @@ def loop():
     keypad.setDebounceTime(50)      #set the debounce time
     while(True):
         key = keypad.getKey()       #obtain the state of keys
-        if(key == keypad.NULL):
-            while true :
+        while(True) :
+            if(key == keypad.NULL):
                 long_string(display, "3 salles..." , 1)
                 long_string(display, "3 ambiances !" , 2)
                 sleep(1.5)
@@ -79,6 +79,8 @@ def loop():
                 long_string(display, "code (15 caract)" , 2)
                 sleep(1.5)
                 display.lcd_clear()
+            if(key != keypad.NULL):
+                break
             
             
         if(key != keypad.NULL):     #if there is key pressed, print its key code.
