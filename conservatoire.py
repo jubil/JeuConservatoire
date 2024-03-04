594f137f58908cfab740daa25f399e0661c7f6f8
Christophe <Christophe@Christophe.fr>
2024-03-04 12:39:39 +0100

    essai changement accueil

--- conservatoire.py
+++ conservatoire.py
@@ -69,6 +69,16 @@ def loop():
     keypad.setDebounceTime(50)      #set the debounce time
     while(True):
         key = keypad.getKey()       #obtain the state of keys
+        while True :
+            if (key == keypad.NULL):
+                long_string(display, "3 salles..." , 1)
+                long_string(display, "3 ambiances !" , 2)
+                sleep(1.5)
+                display.lcd_clear()
+                long_string(display, "Entrez votre code SVP", 1)
+                sleep(1.5)
+                display.lcd_clear()
+                
         if(key != keypad.NULL):     #if there is key pressed, print its key code.
             buzztouches()
             if (key=="D"):
@@ -129,11 +139,11 @@ def loop():
                 sleep(1.5)
                 display.lcd_clear()
                 led_rouge.off()
-                long_string(display, "Jeu des 3 salles" , 1)
-                long_string(display, "Entrez code svp" , 2)
+                #long_string(display, "Jeu des 3 salles" , 1)
+                #long_string(display, "Entrez code svp" , 2)
                 
 # Message d'accueil
-long_string(display, "Jeu des 3 salles" , 1)
-long_string(display, "Entrez code svp" , 2)
+#long_string(display, "Jeu des 3 salles" , 1)
+#long_string(display, "Entrez code svp" , 2)
 
 loop()
