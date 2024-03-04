fedbe261676cf756ca04c00e82720f760bf0b9ac
Christophe <Christophe@Christophe.fr>
2024-03-04 15:51:41 +0100

    retour version ok

--- conservatoire.py
+++ conservatoire.py
@@ -69,18 +69,6 @@ def loop():
     keypad.setDebounceTime(50)      #set the debounce time
     while(True):
         key = keypad.getKey()       #obtain the state of keys
-        while(True) :
-            if(saisie=""):
-                long_string(display, "3 salles..." , 1)
-                long_string(display, "3 ambiances !" , 2)
-                sleep(2)
-                display.lcd_clear()
-                long_string(display, "SVP, saisissez" , 1)
-                long_string(display, "code (15 caract)" , 2)
-                sleep(2)
-                display.lcd_clear()
-                                 
-            
         if(key != keypad.NULL):     #if there is key pressed, print its key code.
             buzztouches()
             if (key=="D"):
@@ -141,6 +129,11 @@ def loop():
                 sleep(1.5)
                 display.lcd_clear()
                 led_rouge.off()
+                long_string(display, "Jeu des 3 salles" , 1)
+                long_string(display, "Entrez code svp" , 2)
                 
+# Message d'accueil
+long_string(display, "Jeu des 3 salles" , 1)
+long_string(display, "Entrez code svp" , 2)
 
 loop()
